6b842a6ea57b4eb6d532b70182daa5e08e899e1e
helloworld2023 <league@wts13.attlocal.net>
2019-04-06 11:28:38 -0700

    Started Lvl 1 Mod 1

---
+++ src/Platypus.java
@@ -0,0 +1,11 @@
+
+public class Platypus {
+	private String name;
+	
+	void sayHi(String name) {
+		System.out.println("The platypus " + name + 
+		" is smarter than your average platypus.");
+		
+		this.name = name;
+	}
+}
---
+++ src/PlatypusRunner.java
@@ -0,0 +1,10 @@
+
+
+public class PlatypusRunner {
+	
+	public static void main(String[] args) {
+		
+		Platypus p = new Platypus();
+		p.sayHi("Erin");
+	}
+}
